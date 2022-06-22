20d7b238cdb1b309a8fb396bef29d8192b31c53b
Emma <boxxu22@gmail.com>
2022-06-22 17:01:24 +0300

    feat:squares scale to fit grid

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ const btn = document.querySelector("#grid-button");
 
 
 btn.addEventListener('click',() => {
-    //TODO: Reject invalid prompt inputs
     x = prompt("please enter the size of grid you want");
+    //TODO: Reject invalid prompt inputs: Not an int and not between 1-100
     createGrid(x);
 })
 
 //Individual Square on the Grid
-function createSquare(i) {
+function createSquare(i,size) {
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     gridSquare.setAttribute('id', "square:"+ i);
 
     //TODO: change square dimensions with grid size
-
+    gridSquare.style.height = 800/size;
+    gridSquare.style.width = 800/size;
+    //Grid-Container sizer /
     //add drawing ability
     gridSquare.addEventListener("mouseover", () => {
         //temporaraily just changes to blue TODO: let user change colour
@@ -28,12 +30,15 @@ function createSquare(i) {
 function createGrid(size) {
     //Delete old grid first
     container.textContent ='';
-    
+    //set dimensions of new grid
+    str = "repeat(" + size + "," + 800/size + "px)";
+    container.style.gridTemplateColumns = str;
+    //fill grid with squares
     const docFrag = document.createDocumentFragment();
 
     let grid = [];
     for (let i = 0; i < (size*size); i++) {
-        grid.push(createSquare(i));
+        grid.push(createSquare(i,size));
         
     }
 
